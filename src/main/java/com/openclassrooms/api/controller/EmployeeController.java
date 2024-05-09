ca3361c564b45a006f84a6746cb0eb3ebc2264c8
Karel <admin@neo7.fr>
2024-05-09 20:33:28 +0200

    Validation

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -1,20 +1,34 @@
 package com.openclassrooms.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.api.model.Employee;
 import com.openclassrooms.api.service.EmployeeService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
   @Autowired
@@ -26,8 +40,9 @@ public class EmployeeController {
    * @param employee An object employee
    * @return The employee object saved
    */
-  @PostMapping("/employee")
-  public Employee createEmployee(@RequestBody Employee employee) {
+  @PostMapping("/")
+  public Employee createEmployee(@Valid @RequestBody Employee employee) {
+    ResponseEntity.ok(employee);
     return employeeService.saveEmployee(employee);
   }
 
@@ -37,7 +52,7 @@ public class EmployeeController {
    * @param id The id of the employee
    * @return An Employee object full filled
    */
-  @GetMapping("/employee/{id}")
+  @GetMapping("/{id}")
   public Employee getEmployee(@PathVariable("id") final Long id) {
     Optional<Employee> employee = employeeService.getEmployee(id);
     if (employee.isPresent()) {
@@ -52,7 +67,7 @@ public class EmployeeController {
    * 
    * @return - An Iterable object of Employee full filled
    */
-  @GetMapping("/employees")
+  @GetMapping("/all")
   public Iterable<Employee> getEmployees() {
     return employeeService.getEmployees();
   }
@@ -64,7 +79,7 @@ public class EmployeeController {
    * @param employee - The employee object updated
    * @return
    */
-  @PutMapping("/employee/{id}")
+  @PutMapping("/{id}")
   public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
     Optional<Employee> e = employeeService.getEmployee(id);
     if (e.isPresent()) {
@@ -100,9 +115,31 @@ public class EmployeeController {
    * 
    * @param id - The id of the employee to delete
    */
-  @DeleteMapping("/employee/{id}")
+  @DeleteMapping("/{id}")
   public void deleteEmployee(@PathVariable("id") final Long id) {
     employeeService.deleteEmployee(id);
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+    JSONObject jo = new JSONObject();
+    jo.put("message", "Validation failed");
+
+    // ALL ERRORS
+    List<String> details = new ArrayList<>();
+    for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+      details.add(error.getDefaultMessage());
+    }
+
+    // FIRST ERROR ONLY (but random order ....)
+    // List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+    // String details = errors.get(0).getDefaultMessage();
+
+    jo.put("details", details);
+
+    return new ResponseEntity(jo.toString(), HttpStatus.BAD_REQUEST);
   }
 
 }
--- src/main/java/com/openclassrooms/api/model/Employee.java
+++ src/main/java/com/openclassrooms/api/model/Employee.java
@@ -6,19 +6,39 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank(message = "First name is required") // Note : sufficient for null, missing and empty value
+  @Size(min = 3, max = 20, message = "First name should be between 3 and 20 characters")
   @Column(name = "first_name")
   private String firstName;
+
+  @NotBlank(message = "Last name is required")
+  @Size(min = 3, max = 20, message = "Last name should be between 3 and 20 characters")
   @Column(name = "last_name")
   private String lastName;
+
+  @NotBlank(message = "Email is required")
+  @Email(message = "Email is invalid")
   private String mail;
+
+  @NotBlank(message = "Password is required")
   private String password;
 }
