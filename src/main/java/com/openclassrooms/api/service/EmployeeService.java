30b0e84f8b03ca641e069fbd50497dc6ccf03dbe
Karel <admin@neo7.fr>
2024-05-14 14:18:37 +0200

    Services as interfaces

--- src/main/java/com/openclassrooms/api/service/EmployeeService.java
+++ src/main/java/com/openclassrooms/api/service/EmployeeService.java
@@ -2,44 +2,17 @@ package com.openclassrooms.api.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.openclassrooms.api.model.Employee;
-import com.openclassrooms.api.model.Ward;
-import com.openclassrooms.api.repository.EmployeeRepository;
 
-@Service
-public class EmployeeService {
+public interface EmployeeService {
 
-  @Autowired
-  private EmployeeRepository employeeRepository;
+  public Optional<Employee> getEmployee(final Long id);
 
-  public Optional<Employee> getEmployee(final Long id) {
-    return employeeRepository.findById(id);
-  }
+  public Iterable<Employee> getEmployees();
 
-  public Iterable<Employee> getEmployees() {
-    return employeeRepository.findAll();
-  }
+  public void deleteEmployee(final Long id);
 
-  public void deleteEmployee(final Long id) {
-    employeeRepository.deleteById(id);
-  }
+  public Employee saveEmployee(Employee employee);
 
-  public Employee saveEmployee(Employee employee) {
-    for (Ward w : employee.getWards()) {
-      w.setEmployee(employee);
-    }
-    return employeeRepository.save(employee);
-  }
-
-  public Employee copyEmployee(Employee fromEmployee, Employee toEmployee) {
-    toEmployee.setFirstName(fromEmployee.getFirstName());
-    toEmployee.setLastName(fromEmployee.getLastName());
-    toEmployee.setMail(fromEmployee.getMail());
-    toEmployee.setPassword(fromEmployee.getPassword());
-    toEmployee.setWards(fromEmployee.getWards());
-    return toEmployee;
-  }
+  public Employee copyEmployee(Employee fromEmployee, Employee toEmployee);
 }
---
+++ src/main/java/com/openclassrooms/api/service/serviceImpl/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.api.service.serviceImpl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.api.model.Employee;
+import com.openclassrooms.api.model.Ward;
+import com.openclassrooms.api.repository.EmployeeRepository;
+import com.openclassrooms.api.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  public Optional<Employee> getEmployee(final Long id) {
+    return employeeRepository.findById(id);
+  }
+
+  public Iterable<Employee> getEmployees() {
+    return employeeRepository.findAll();
+  }
+
+  public void deleteEmployee(final Long id) {
+    employeeRepository.deleteById(id);
+  }
+
+  public Employee saveEmployee(Employee employee) {
+    for (Ward w : employee.getWards()) {
+      w.setEmployee(employee);
+    }
+    return employeeRepository.save(employee);
+  }
+
+  public Employee copyEmployee(Employee fromEmployee, Employee toEmployee) {
+    toEmployee.setFirstName(fromEmployee.getFirstName());
+    toEmployee.setLastName(fromEmployee.getLastName());
+    toEmployee.setMail(fromEmployee.getMail());
+    toEmployee.setPassword(fromEmployee.getPassword());
+    toEmployee.setWards(fromEmployee.getWards());
+    return toEmployee;
+  }
+}
