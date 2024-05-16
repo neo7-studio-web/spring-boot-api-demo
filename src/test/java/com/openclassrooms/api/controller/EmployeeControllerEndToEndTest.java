a27f53309856ac3d82784cd087c448119e8ffc1d
Karel <admin@neo7.fr>
2024-05-16 14:43:01 +0200

    Minor : warning fixes

--- src/test/java/com/openclassrooms/api/controller/EmployeeControllerEndToEndTest.java
+++ src/test/java/com/openclassrooms/api/controller/EmployeeControllerEndToEndTest.java
@@ -1,22 +1,15 @@
 package com.openclassrooms.api.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class EmployeeControllerEndToEndTest {
 
-  @Autowired
-  private MockMvc mockMvc;
+  // @Autowired
+  // private MockMvc mockMvc;
 
   @Test
   public void testGetEmployeesEndToEnd() throws Exception {
