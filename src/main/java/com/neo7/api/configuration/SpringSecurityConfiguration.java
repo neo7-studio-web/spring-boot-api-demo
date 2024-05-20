41abd2d90ed92dc9da9c247a7cfbc313d35dca63
Karel <admin@neo7.fr>
2024-05-20 13:58:53 +0200

    JWT key as property instead of hard coded

--- src/main/java/com/neo7/api/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/neo7/api/configuration/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.neo7.api.configuration;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,8 +24,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @EnableWebSecurity
+@ConfigurationProperties(prefix = "neo7.security")
 public class SpringSecurityConfiguration {
 
   @Autowired
@@ -63,17 +68,24 @@ public class SpringSecurityConfiguration {
     return new BCryptPasswordEncoder();
   }
 
-  private String jwtKey = "D7tIq+rkB7G2+EZ8hOlJ4YKpVq9DlO3K1wqjufp3JlrN58G3G/KkM+HumRqGiXW8a6ZB1TNhGbyhLq8zAW4ZsvpobwH4G6/P7L4vGWK27awt45cKc3JEGirFlXpSI5Zqds17GL1FIv5+pOeybTpfGTTv3iKwkKe6JembjZBqDdJs6inY3t7P8qmzvA9RwF2JpSYNeL/No5R/h04yuQ7c6Ny7+bAU2fgMH4iOI8Qn0vVoIuAWHLa1FM0DrERkoZoTQy/kLGiiQDCmxRiglnhSn5pjou5iMB/d5oRzkArqUqV3JTwcei9yVCXErKFyh4PjRWi3wtZSPTk5Bo6LF/9NdEcWyvltN2kUWDeCGkNxc3o=";
+  /**
+   * Value is coming from application.properties and automatically injected
+   * by Spring because of @ConfigurationProperties above and prefix
+   */
+  @Setter
+  @Getter
+  private String jwtKey;
 
   @Bean
   public JwtDecoder jwtDecoder() {
-    SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+    SecretKeySpec secretKey = new SecretKeySpec(this.getJwtKey().getBytes(), 0, this.getJwtKey().getBytes().length,
+        "RSA");
     return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
   }
 
   @Bean
   public JwtEncoder jwtEncoder() {
-    return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
+    return new NimbusJwtEncoder(new ImmutableSecret<>(this.getJwtKey().getBytes()));
   }
 
 }
